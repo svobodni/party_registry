9e93365a90ea22e3e37dc4cee04890978193fa03
Jiří Kubíček <jiri@kubicek.cz>
2014-02-18 23:43:43 +0100

    Generated Rails 4.0.3 skeleton
    # rails new party_registry

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PartyRegistry::Application.config.secret_key_base = '860a1498ed28a7a08e8a6a48f9e9dc1f99d4398bb26e4e8432466916be491f2b7e9ec562151f1d6f49993b5bb8ba29fa2f28d88df9de831d96536564ae16be48'
