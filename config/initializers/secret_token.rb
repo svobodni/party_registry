f0bd56b3536d4fb9b6f149b652805918da033fb8
Jiří Kubíček <jiri.kubicek@kraxnet.cz>
2014-08-25 22:54:37 +0200

    Konfigurovatelný secret key

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,5 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-PartyRegistry::Application.config.secret_key_base = '860a1498ed28a7a08e8a6a48f9e9dc1f99d4398bb26e4e8432466916be491f2b7e9ec562151f1d6f49993b5bb8ba29fa2f28d88df9de831d96536564ae16be48'
+PartyRegistry::Application.config.secret_key_base = configatron.secret_key_base
+
