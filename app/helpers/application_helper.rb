f0cbda780852e26ba5707cff76745cc6fccbec9d
Jiří Kubíček <jiri.kubicek@kraxnet.cz>
2014-11-22 00:01:47 +0100

    Odkazy na podepsanou přihlášku

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,14 @@
+# -*- encoding : utf-8 -*-
 module ApplicationHelper
   def page_title(value)
     @page_title = value
+  end
+
+  def link_to_member_application(member)
+	if member.signed_application
+		link_to "Podepsaná přihláška", signed_application_person_path(member, format: :pdf)
+	else
+		link_to "Přihláška", application_person_path(member, format: :pdf)
+	end
   end
 end
