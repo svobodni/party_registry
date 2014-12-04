237442003a23b0fa92989d3766431218ba714f6d
Jiří Kubíček <jiri.kubicek@kraxnet.cz>
2014-12-05 00:58:51 +0100

    Datatable všude (paginace, vyhledávání v tabulkách)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,6 +12,37 @@ module ApplicationHelper
   end
   end
 
+  def datatable(id)
+    content_tag :script do
+      "$(document).ready(function() {
+        $('##{id}').DataTable( {
+           \"language\": {
+             \"sProcessing\":   \"Provádím...\",
+             \"sLengthMenu\":   \"Zobraz záznamů _MENU_\",
+             \"sZeroRecords\":  \"Žádné záznamy nebyly nalezeny\",
+             \"sInfo\":         \"Zobrazuji _START_ až _END_ z celkem _TOTAL_ záznamů\",
+             \"sInfoEmpty\":    \"Zobrazuji 0 až 0 z 0 záznamů\",
+             \"sInfoFiltered\": \"(filtrováno z celkem _MAX_ záznamů)\",
+             \"sInfoPostFix\":  \"\",
+             \"sSearch\":       \"Hledat:\",
+             \"sUrl\":          \"\",
+             \"oPaginate\": {
+                \"sFirst\":    \"První\",
+                \"sPrevious\": \"Předchozí\",
+                \"sNext\":     \"Další\",
+                \"sLast\":     \"Poslední\"
+             }
+          }
+        } );
+       } );".html_safe
+    end
+  end
+
+
+
+
+
+
   def tel_to(number=nil)
     if number
       phone = number.gsub(/\.- /,'')
