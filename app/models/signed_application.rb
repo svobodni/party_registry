782de8a884abf8e39f4fb932538c5a6f7bd0f0b7
Jiří Kubíček <jiri.kubicek@kraxnet.cz>
2015-02-27 11:23:02 +0100

    Povolení divného content type u nahrané přihlášky

--- app/models/signed_application.rb
+++ app/models/signed_application.rb
@@ -2,7 +2,7 @@ class SignedApplication < ActiveRecord::Base
   belongs_to :person
 
   has_attached_file :scan, path: ":rails_root/data/signed_applications/:person_id.pdf", url: "/people/:person_id/signed_application.pdf"
-  validates_attachment :scan, content_type: { content_type: "application/pdf" }
+  validates_attachment :scan, content_type: { content_type: ["application/pdf", "application/vnd.ms-excel"] }
 
   validates :person_id, uniqueness: true
 
