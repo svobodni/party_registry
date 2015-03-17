825c0ed52d94ce83fdca908d17f1e827d01f4e4f
Jiří Kubíček <jiri.kubicek@kraxnet.cz>
2015-03-18 00:54:37 +0100

    NewsFeed odolný proti výpadkům

--- app/models/news_feed.rb
+++ app/models/news_feed.rb
@@ -1,13 +1,17 @@
 class NewsFeed
 
   def self.items
-    client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = configatron.twitter.consumer_key
-      config.consumer_secret     = configatron.twitter.consumer_secret
-      config.access_token        = configatron.twitter.access_token
-      config.access_token_secret = configatron.twitter.access_token_secret
+    begin
+      client = Twitter::REST::Client.new do |config|
+        config.consumer_key        = configatron.twitter.consumer_key
+        config.consumer_secret     = configatron.twitter.consumer_secret
+        config.access_token        = configatron.twitter.access_token
+        config.access_token_secret = configatron.twitter.access_token_secret
+      end
+      client.home_timeline
+    rescue StandardError => e
+      []
     end
-    client.home_timeline
   end
 
 end
