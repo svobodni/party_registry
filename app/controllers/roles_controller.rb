4f5629b0252b07ed374cc3cb8ca3d6a9ddd62615
Jiří Kubíček <jiri.kubicek@kraxnet.cz>
2015-04-03 22:58:47 +0200

    Oprava vytváření rolí přes API

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -39,7 +39,7 @@ class RolesController < ApplicationController
     respond_to do |format|
       if @role.save
         format.html { redirect_to roles_url, notice: 'Role was successfully created.' }
-        format.json { render json: @role, status: :created, location: @role }
+        format.json { render json: @role, status: :created, location: role_path(@role) }
       else
         format.html { render action: 'new' }
         format.json { render json: @role.errors, status: :unprocessable_entity }
