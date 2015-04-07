e3fcf83b99d22dae6c5c7b4972c82187a765d0a6
Jiří Kubíček <jiri.kubicek@kraxnet.cz>
2015-04-07 23:42:16 +0200

    U adres k dohledání nezobrazujeme nezaplacené příznivce

--- app/controllers/backoffice/people_controller.rb
+++ app/controllers/backoffice/people_controller.rb
@@ -12,7 +12,7 @@ class Backoffice::PeopleController < ApplicationController
   end
 
   def with_unknown_address
-    @people = Person.includes([:domestic_ruian_address]).select{|p| p.domestic_ruian_address.nil?}
+    @people = Person.includes([:domestic_ruian_address]).select{|p| p.domestic_ruian_address.nil?}.reject{|p| p.supporter_status=="registered"}
   end
 
   def without_signed_application
