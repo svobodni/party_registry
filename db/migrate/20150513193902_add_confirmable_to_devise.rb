20edee952d07a7a1f10966db6661d66e475394cc
Jiří Kubíček <jiri.kubicek@kraxnet.cz>
2015-05-14 00:31:24 +0200

    Rozšíření modelu osoby o confirmable a lockable

---
+++ db/migrate/20150513193902_add_confirmable_to_devise.rb
@@ -0,0 +1,13 @@
+class AddConfirmableToDevise < ActiveRecord::Migration
+  def up
+    add_column :people, :confirmation_token, :string
+    add_column :people, :confirmed_at, :datetime
+    add_column :people, :confirmation_sent_at, :datetime
+    add_index :people, :confirmation_token, unique: true
+    execute("UPDATE people SET confirmed_at = NOW()")
+  end
+
+  def down
+    remove_columns :people, :confirmation_token, :confirmed_at, :confirmation_sent_at
+  end
+end
---
+++ db/migrate/20150513195929_add_locakable_to_devise.rb
@@ -0,0 +1,11 @@
+class AddLocakableToDevise < ActiveRecord::Migration
+  def up
+    add_column :people, :failed_attempts, :integer, default: 0
+    add_column :people, :unlock_token, :string
+    add_column :people, :locked_at, :datetime
+  end
+
+  def down
+    remove_columns :people, :locked_at, :failed_attempts, :unlock_token
+  end
+end
