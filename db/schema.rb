cb647c8bbe7a3ef979c8d2d7a59a6493205d9d88
Jiří Kubíček <jiri.kubicek@kraxnet.cz>
2015-06-20 14:05:38 +0200

    [EVENTS] event ma jmeno

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150613214936) do
+ActiveRecord::Schema.define(version: 20150620110655) do
 
   create_table "bank_payments", force: true do |t|
     t.integer  "account_id"
