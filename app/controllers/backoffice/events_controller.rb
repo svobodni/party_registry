6cd6ac7d4a8eb0a3c443d11a8dbb3c9e4cc2fa13
Jiří Kubíček <jiri.kubicek@kraxnet.cz>
2015-10-30 22:45:22 +0100

    Výpis událostí podle ID osoby nebo typu události

--- app/controllers/backoffice/events_controller.rb
+++ app/controllers/backoffice/events_controller.rb
@@ -3,7 +3,14 @@ class Backoffice::EventsController < ApplicationController
   before_action :authorize_backoffice
 
   def index
-    @events = Event.last(20).reverse
+    @event=Event.new()
+    @events = Event.all
+    if params[:event]
+      @event=Event.new(command: params[:event][:command], eventable_id: params[:event][:eventable_id])
+      @events = @events.where(eventable_id: params[:event][:eventable_id], eventable_type: 'Person') unless params[:event][:eventable_id].blank?
+      @events = @events.where(command: params[:event][:command]) unless params[:event][:command].blank?
+    end
+    @events = @events.last(20).reverse
   end
 
   def show
