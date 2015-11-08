61590fc903abf4ce629db3ae90a4d786dab22e95
Jiří Kubíček <jiri.kubicek@kraxnet.cz>
2015-11-08 20:26:36 +0100

    Události hledáme podle názvu a ne podle příkazu

--- app/controllers/backoffice/events_controller.rb
+++ app/controllers/backoffice/events_controller.rb
@@ -6,9 +6,9 @@ class Backoffice::EventsController < ApplicationController
     @event=Event.new()
     @events = Event.all
     if params[:event]
-      @event=Event.new(command: params[:event][:command], eventable_id: params[:event][:eventable_id])
+      @event=Event.new(name: params[:event][:name], eventable_id: params[:event][:eventable_id])
       @events = @events.where(eventable_id: params[:event][:eventable_id], eventable_type: 'Person') unless params[:event][:eventable_id].blank?
-      @events = @events.where(command: params[:event][:command]) unless params[:event][:command].blank?
+      @events = @events.where(name: params[:event][:name]) unless params[:event][:name].blank?
     end
     @events = @events.order('created_at DESC').paginate(:page => params[:page], :per_page => 20)
   end
