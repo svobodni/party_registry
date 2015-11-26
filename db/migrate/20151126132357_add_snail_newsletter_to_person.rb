7fb609a3162da9d974c4bd29ee6a44d04c1638e4
Jiri Kubicek <jiri.kubicek@kraxnet.cz>
2015-11-26 16:52:59 +0100

    Odběr Berana může administrovat i kancelář

--- db/migrate/20151126132357_add_snail_newsletter_to_person.rb
+++ db/migrate/20151126132357_add_snail_newsletter_to_person.rb
@@ -1,5 +1,5 @@
 class AddSnailNewsletterToPerson < ActiveRecord::Migration
   def change
-    add_column :people, :snail_newsletter, :boolean
+    add_column :people, :snail_newsletter, :boolean, default: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -183,13 +183,13 @@ ActiveRecord::Schema.define(version: 20151126132357) do
     t.string   "photo_url",                  limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "email",                      limit: 255,   default: "", null: false
-    t.string   "encrypted_password",         limit: 255,   default: "", null: false
-    t.string   "password_salt",              limit: 255,   default: "", null: false
+    t.string   "email",                      limit: 255,   default: "",   null: false
+    t.string   "encrypted_password",         limit: 255,   default: "",   null: false
+    t.string   "password_salt",              limit: 255,   default: "",   null: false
     t.string   "reset_password_token",       limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",              limit: 4,     default: 0,  null: false
+    t.integer  "sign_in_count",              limit: 4,     default: 0,    null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip",         limit: 255
