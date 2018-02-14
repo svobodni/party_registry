9e6319955f6508e323e7667d189664f1ff197a1a
Jiri Kubicek <jiri.kubicek@kraxnet.cz>
2018-02-14 17:07:28 +0100

    Oprávnění pro kancelář

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -61,7 +61,7 @@ class Ability
       end
 
       # Speciální role pro kancelář
-      if ([1, 86, 342, 344, 2804, 3860].member?(user.id) || user.roles.detect{|r| r.body.try(:acronym)=="ReP"})
+      if ([1, 86, 342, 344, 2804, 3860, 5679, 7873].member?(user.id) || user.roles.detect{|r| r.body.try(:acronym)=="ReP"})
         can [:read, :application], Person
         can :upload, SignedApplication
         can [:create, :destroy], Role
