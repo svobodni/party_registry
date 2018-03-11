0b1325263111f6f3c671663a125abce9de69f2cf
Jiri Kubicek <jiri.kubicek@kraxnet.cz>
2018-03-11 22:46:23 +0100

    notifier fix

--- app/models/daily_notifier.rb
+++ app/models/daily_notifier.rb
@@ -1,7 +1,7 @@
 class DailyNotifier
 
   def self.notify_regional_presidiums
-    data = Event.where("created_at >= ?", Time.now - 1.day).where(eventable_type: "Person", command: "delete").group_by{|a| a.previous_data["domestic_region_id"]}
+    data = Event.where("created_at >= ?", Time.now - 1.day).where(eventable_type: "Person", command: "DeletePerson").group_by{|a| a.previous_data["domestic_region_id"]}
     data.each{|region_id, events|
       region = Region.find(region_id)
       PresidiumNotifications.daily_event_notifier(region, events).deliver
@@ -9,7 +9,7 @@ class DailyNotifier
   end
 
   def self.notify_branch_coordinators
-    data = Event.where("created_at >= ?", Time.now - 1.day).where(eventable_type: "Person", command: "delete").group_by{|a| a.previous_data["domestic_branch_id"]}
+    data = Event.where("created_at >= ?", Time.now - 1.day).where(eventable_type: "Person", command: "DeletePerson").group_by{|a| a.previous_data["domestic_branch_id"]}
     data.each{|branch_id, events|
       if branch_id
         branch = Branch.find(branch_id)
