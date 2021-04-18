b42fa5a91d8f7584f3eb27a111cc2de143d76e14
Jiří Kubíček <jiri.kubicek@kraxnet.cz>
2021-04-18 17:50:17 +0200

    Rails 5.2.5 gem&app:update

--- config/application.rb
+++ config/application.rb
@@ -12,8 +12,6 @@ module PartyRegistry
     config.load_defaults 5.1
 
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
@@ -48,5 +46,8 @@ module PartyRegistry
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
   end
 end
