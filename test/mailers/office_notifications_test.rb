7bd63e58f48b724162f715a9a59c81d39b7ecc8a
Jiří Kubíček <jiri.kubicek@kraxnet.cz>
2021-04-19 16:26:58 +0200

    oprava notifikačního testu

--- test/mailers/office_notifications_test.rb
+++ test/mailers/office_notifications_test.rb
@@ -2,11 +2,11 @@ require 'test_helper'
 
 class OfficeNotificationsTest < ActionMailer::TestCase
   test "membership_request_rejected" do
-    mail = OfficeNotifications.membership_request_rejected
-    assert_equal "Membership request rejected", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    mail = OfficeNotifications.membership_request_rejected(FactoryBot.create(:registered_requesting_membership))
+    assert_equal "Zamítnuté členství - vraťte platbu", mail.subject
+    assert_equal ["kancelar@svobodni.cz"], mail.to
+    assert_equal ["kancelar@svobodni.cz"], mail.from
+    assert_match "vyznačilo odmítnutí zájemce", mail.body.encoded
   end
 
 end
