36bd40bfb72f14bd5afbbe611c6b439c7dccd2a4
Jiří Kubíček <jiri.kubicek@kraxnet.cz>
2021-04-19 12:24:31 +0200

    finance api test

--- test/controllers/finance_api_controller_test.rb
+++ test/controllers/finance_api_controller_test.rb
@@ -9,19 +9,26 @@ class FinanceApiControllerTest < ActionController::TestCase
     end
 
     should "get info on membership_fee payment" do
-      @person = FactoryBot.create(:member_awaiting_first_payment)
-      post :payments, format: :json, id: @person.vs
+      @person = FactoryBot.create(:registered)
+      @person.create_membership_request(membership_requested_on: "2018-01-01")
+      post :membership_payments, format: :json, params: {id: @person.vs}
       assert_response :success
       body = JSON.parse(response.body)
-      assert_equal "Členský příspěvek - #{@person.name}", body["payment"]["accounting_note"]
+      assert_equal @person.name, body["payment"]["name"]
+      assert_equal @person.domestic_region.id, body["payment"]["region_id"]
     end
 
     should "get info on supporter fee payment" do
       @person = FactoryBot.create(:supporter)
-      post :payments, format: :json, id: @person.vs
+      post :donation_payments, format: :json, params: {id: @person.vs}
       assert_response :success
       body = JSON.parse(response.body)
-      assert_equal "Příspěvek příznivce - #{@person.name}", body["payment"]["accounting_note"]
+      assert_equal @person.name, body["payment"]["name"]
+      assert_equal @person.domestic_address_street, body["payment"]["street"]
+      assert_equal @person.domestic_address_city, body["payment"]["city"]
+      assert_equal @person.domestic_address_zip, body["payment"]["zip"]
+      assert_equal @person.email, body["payment"]["email"]
+      assert_equal @person.date_of_birth.to_s, body["payment"]["date_of_birth"]
     end
 
   end
